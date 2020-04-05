c6f998e6b86c143643e62e3aeeabdda84f5c986b
Only Wan <lazuardojuan@gmail.com>
2020-04-05 12:23:36 +0700

    Membuat Tampilan Awal

---
+++ app/src/main/java/com/example/tugasakb/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tugasakb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+ NIM    : 10117160
+ Nama   : Juan Lazuardo
+ Tugas  : AKB Pemananasan 1
+
+ Update :
+    - 4 April 2020 = Membuat Tampilan Awal
+
+*/
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
