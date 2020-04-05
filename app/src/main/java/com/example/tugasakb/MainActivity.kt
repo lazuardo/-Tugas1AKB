1b5d351a5266dfe393ee35b70cedf39e02c89f72
Only Wan <lazuardojuan@gmail.com>
2020-04-05 13:20:02 +0700

    Membuat Tampilan Login

--- app/src/main/java/com/example/tugasakb/MainActivity.kt
+++ app/src/main/java/com/example/tugasakb/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.tugasakb
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 /*
  NIM    : 10117160
@@ -9,13 +12,18 @@ import android.os.Bundle
  Tugas  : AKB Pemananasan 1
 
  Update :
-    - 4 April 2020 = Membuat Tampilan Awal
+    - 4 April 2020 = Membuat Tampilan Awal ( Welcome)
+    - 5 April 2020 = membuat Tampilan 2 ( Login )
 
 */
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.buttonmulai)
+        button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
