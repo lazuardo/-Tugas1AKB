1af5351d2ca1af53d6498baedb78510892fc54c2
Only Wan <lazuardojuan@gmail.com>
2020-04-07 13:47:32 +0700

    Release APK

--- app/src/main/java/com/example/tugasakb/MainActivity.kt
+++ app/src/main/java/com/example/tugasakb/MainActivity.kt
@@ -9,12 +9,14 @@ import android.widget.Button
 /*
  NIM    : 10117160
  Nama   : Juan Lazuardo
+ Kelas  : AKB-4 / IF-4
  Tugas  : AKB Pemananasan 1
 
  Update :
     - 4 April 2020 = Membuat Tampilan Awal ( Welcome)
 
-    - 5 April 2020 = Membuat Tampilan 2 ( Login ) + upload github
+    - 5 April 2020 = - Membuat Tampilan 2 ( Login )
+                     - Upload github
 
     - 6 April 2020 = - Membuat Tampilan 3 (Biodata)
                      - Membuat EditText pada pengisian Kode Keluarga menjadi CAPSLOCK
@@ -22,10 +24,9 @@ import android.widget.Button
                      - Merubah Icon Apikasi
                      - Update Github
 
-    - 7 April 2020 = Membuat GetText dan SetText untuk textView di halaman Say Hai agar kata " Kamu "
-                     terganti dengan inout text di halaman biodata.
-
-
+    - 7 April 2020 = - Membuat GetText dan SetText untuk textView di halaman Say Hai agar kata " Kamu "
+                       terganti dengan inout text di halaman biodata.
+                     - Merilis APK dengan Generate Signed APK
 
 */
 class MainActivity : AppCompatActivity() {
